bac726d0b81ccf6db11a5c32eeab3e074d6786a0
hejiecheng <1196298988@qq.com>
2019-04-28 09:55:08 +0800

    init project

---
+++ src/main/java/com/netease/focusmonk/FocusMonkApplication.java
@@ -0,0 +1,13 @@
+package com.netease.focusmonk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusMonkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusMonkApplication.class, args);
+    }
+
+}
