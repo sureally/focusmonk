3e90be0d0996479d73fdaa0c3be3e8a4d6c31ae5
Koilicrest <koilicrest@qq.com>
2019-04-29 14:45:51 +0800

    修改配置文件 application.properties 以及添加各阶段的配置文件。

--- src/test/java/com/netease/focusmonk/khf/MergeSpringBootMybatisTest.java
+++ src/test/java/com/netease/focusmonk/khf/MergeSpringBootMybatisTest.java
@@ -3,19 +3,20 @@ package com.netease.focusmonk.khf;
 import com.netease.focusmonk.dao.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MergeSpringBootMybatisTest {
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Test
     public void testMerge() {
-
+        userMapper.selectByPrimaryKey(1);
     }
 }
