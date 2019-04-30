d8c79d6d35149404ba80cf89824e9f7ec2c7d716
wenjun shu <shu_wenjun@qq.com>
2019-04-30 16:35:00 +0800

    剔除自测尝试代码。

--- src/test/java/com/netease/focusmonk/swj/service/WhiteNoiseSchemeServiceImplTest.java
+++ src/test/java/com/netease/focusmonk/swj/service/WhiteNoiseSchemeServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.netease.focusmonk.swj.service;
 
-import com.netease.focusmonk.common.JsonResult;
 import com.netease.focusmonk.service.WhiteNoiseSchemeServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @ClassName com.netease.focusmonk.swj.service.WhiteNoiseSchemeServiceImplTest
@@ -27,13 +24,7 @@ public class WhiteNoiseSchemeServiceImplTest {
 
     @Test
     public void otherTest() {
-        List<String> list = new ArrayList<>();
-        list.add("abs");
-        list.add(null);
-        for (String str : list) {
-            System.out.println(str.length());
-        }
-        System.out.println(JsonResult.getErrorResult(list));
+
     }
 
 }
