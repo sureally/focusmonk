4ca16ed8a23c65e76acd9cec1b4fb03f269c0065
wenjun shu <shu_wenjun@qq.com>
2019-05-08 19:44:05 +0800

    RedisConstant将home替换room(修复)

--- src/main/java/com/netease/focusmonk/common/RedisConstant.java
+++ src/main/java/com/netease/focusmonk/common/RedisConstant.java
@@ -8,13 +8,18 @@ package com.netease.focusmonk.common;
  * @Version 1.0
  **/
 public interface RedisConstant {
-    // redis key 的构建前缀
-    String PREDIX_HOME = "room";
+    /**
+     *  使用下面的常量拼接相应的 redis key。
+     *  用户信息 key: PREDIX_ROOM + "_" + roomId + "_" + PREDIX_USER + "_" + userId
+     *  房间信息 key: PREDIX_ROOM + "_" + roomId
+     *  用户是否在房间内: PREDIX_INROOM + "_" + roomId
+     */
+    String PREDIX_ROOM = "room";
     String PREDIX_USER = "user";
-    String PREDIX_INHOME = "inroom";
+    String PREDIX_INROOM = "inroom";
 
-    // 用户信息valued对应的hashkey常量
-    String USER_HOME_ID = "userRoomId";
+    // 用户信息 value 对应的 hashkey 常量
+    String USER_ROOM_ID = "userRoomId";
     String START_TIME = "starTime";
     String START_REST_TIME = "startRestTime";
     String REST_TIME = "restTime";
