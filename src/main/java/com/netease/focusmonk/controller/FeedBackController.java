4b4f1ae658ca1b6c3e62649d15ab5ae4e48edc36
hejiecheng <1196298988@qq.com>
2019-05-13 11:25:46 +0800

    修改jwt

--- src/main/java/com/netease/focusmonk/controller/FeedBackController.java
+++ src/main/java/com/netease/focusmonk/controller/FeedBackController.java
@@ -1,10 +1,8 @@
 package com.netease.focusmonk.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.netease.focusmonk.common.JsonResult;
 import com.netease.focusmonk.common.ResultCode;
 import com.netease.focusmonk.service.FeedBackServiceImpl;
-import com.netease.focusmonk.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,18 +24,20 @@ public class FeedBackController {
         this.feedBackService = feedBackService;
     }
 
+    /**
+     * 反馈意见接口
+     * @param info
+     * @param userId
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/info", method = RequestMethod.POST)
     public JsonResult feedBackInfo(@RequestParam(value = "info") String info,
-                                   @RequestParam(value = "jwt") String jwt) throws Exception {
-        String jwtJson = JWTUtil.parseJWT(jwt).getBody().getSubject();
-        JSONObject sessionInfo = JSONObject.parseObject(jwtJson);
-        String userId = sessionInfo.getString("userId");
-        if (userId == null || userId.isEmpty()) {
-            return JsonResult.getCustomResult(ResultCode.JWT_ERROR);
-        }
+                                   @RequestParam(value = "userId") String userId) throws Exception {
         if (info == null || info.isEmpty()) {
             return JsonResult.getCustomResult(ResultCode.PARAM_ERROR);
         }
+        info = info.length() > 200 ? info.substring(0,200) : info;
         if (feedBackService.addFeedBackInfo(userId, info)) {
             return JsonResult.getSuccessResult();
         } else {
