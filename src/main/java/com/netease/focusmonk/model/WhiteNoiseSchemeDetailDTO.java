746c1aede4beec85d98caa36766a43021194835e
wenjun shu <shu_wenjun@qq.com>
2019-05-13 16:28:43 +0800

    白噪声接口改进。

--- src/main/java/com/netease/focusmonk/model/WhiteNoiseSchemeDetailDTO.java
+++ src/main/java/com/netease/focusmonk/model/WhiteNoiseSchemeDetailDTO.java
@@ -6,7 +6,7 @@ import lombok.ToString;
 
 /**
  * @ClassName com.netease.focusmonk.model.WhiteNoiseSchemeDetailDTO
- * @Desciption
+ * @Desciption 返回前端中的WhiteNoiseSchemeDetail，增加了元素name，但是这个增加了服务器的压力。
  * @Author Shu WJ
  * @DateTime 2019-05-13 14:56
  * @Version 1.0
